0641055b9c6e2aee1235b79b56f579db819edd41
Luiz <luzdrik0@gmail.com>
2026-07-13 20:33:44 -0300

    refactor: simplificar lógica com dicionários de operações

--- calculadora.py
+++ calculadora.py
@@ -4,21 +4,24 @@ import operations
 import inputs
 
 programa = True
-print("\n==================================\n" \
-"Bem-vindo(a) à calculadora Python!\n" \
-"==================================")
+
+BOAS_VINDAS = """
+==================================
+Bem-vindo(a) à calculadora Python!
+==================================
+"""
 
 MENU = """ 
-Escolha a operação desejada:\n \
-1 - Adição\n \
-2 - Subtração\n \
-3 - Multiplicação\n \
-4 - Divisão\n \
-5 - Potenciação\n \
-6 - Radiciação\n \
-7 - Divisão inteira\n \
-8 - Resto da Divisão\n \
-0 - Sair
+Escolha a operação desejada:
+ 1 - Adição
+ 2 - Subtração
+ 3 - Multiplicação
+ 4 - Divisão
+ 5 - Potenciação
+ 6 - Radiciação
+ 7 - Divisão inteira 
+ 8 - Resto da Divisão
+ 0 - Sair
 """
 
 operacoes = {
@@ -32,6 +35,18 @@ operacoes = {
     8: operations.divisao_resto
 }
 
+nomes_operacoes = {
+    1: "ADIÇÃO",
+    2: "SUBTRAÇÃO",
+    3: "MULTIPLICAÇÃO",
+    4: "DIVISÃO",
+    5: "POTENCIAÇÃO",
+    6: "RADICIAÇÃO",
+    7: "DIVISÃO INTEIRA",
+    8: "RESTO DA DIVISÃO"
+}
+
+print(BOAS_VINDAS)
 while programa:
     print(MENU)
     opcao = inputs.ler_inteiro("-> Opção: ")
@@ -40,52 +55,19 @@ while programa:
         print("\nAté a próxima! :)")
         programa = False
 
-    elif opcao == 1:
-        print("\n=== OPERAÇÃO: ADIÇÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[1](num1,num2)
+    elif opcao in operacoes:
+        
+        print(f"\n=== OPERAÇÃO: {nomes_operacoes[opcao]} ===")
+
+        if opcao == 6:
+            num1 = inputs.ler1()
+            resultado = operacoes[opcao](num1)
+
+        else:
+            num1, num2 = inputs.ler2()
+            resultado = operacoes[opcao](num1,num2)
+        
         print("Resultado: ", resultado)
 
-    elif opcao == 2:
-        print("\n=== OPERAÇÃO: SUBTRAÇÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[2](num1,num2)
-        print("Resultado: ", resultado)
-
-    elif opcao == 3:
-        print("\n=== OPERAÇÃO: MULTIPLICAÇÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[3](num1,num2)
-        print("Resultado: ", resultado)
-
-    elif opcao == 4:
-        print("\n=== OPERAÇÃO: DIVISÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[4](num1,num2)
-        print("Resultado: ", resultado)
-
-    elif opcao == 5:
-        print("\n=== OPERAÇÃO: POTENCIAÇÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[5](num1,num2)
-        print("Resultado: ", resultado)
-
-    elif opcao == 6:
-        print("\n=== OPERAÇÃO: RADICIAÇÃO ===")
-        num1 = inputs.ler1()
-        resultado = operacoes[6](num1)
-        print("Resultado: ", round(resultado, 2))
-
-    elif opcao == 7:
-        print("\n=== OPERAÇÃO: DIVISÃO INTEIRA ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[7](num1,num2)
-        print("Resultado: ", resultado)
-
-    elif opcao == 8:
-        print("\n=== OPERAÇÃO: RESTO DA DIVISÃO ===")
-        num1, num2 = inputs.ler2()
-        resultado = operacoes[8](num1,num2)
-        print("Resultado: ", resultado)
     else:
         print("Opção inválida.")
