524003cde84e7475f2f4d92561ee00b25582915f
Luiz <luzdrik0@gmail.com>
2026-07-13 18:00:11 -0300

    refactor: criar estrutura de operações com dicionário

--- calculadora.py
+++ calculadora.py
@@ -8,25 +8,34 @@ print("\n==================================\n" \
 "Bem-vindo(a) à calculadora Python!\n" \
 "==================================")
 
-menu = ("\nEscolha a operação desejada:\n" \
-"1 - Adição\n" \
-"2 - Subtração\n" \
-"3 - Multiplicação\n" \
-"4 - Divisão\n" \
-"5 - Potenciação\n" \
-"6 - Radiciação\n" \
-"7 - Divisão inteira\n" \
-"8 - Resto da Divisão\n" \
-"0 - Sair")
+MENU = """ 
+Escolha a operação desejada:\n \
+1 - Adição\n \
+2 - Subtração\n \
+3 - Multiplicação\n \
+4 - Divisão\n \
+5 - Potenciação\n \
+6 - Radiciação\n \
+7 - Divisão inteira\n \
+8 - Resto da Divisão\n \
+0 - Sair
+"""
+
+operacoes = {
+    1: operations.soma,
+    2: operations.subtracao,
+    3: operations.multiplicacao,
+    4: operations.divisao,
+    5: operations.potenciacao,
+    6: operations.radiciacao,
+    7: operations.divisao_inteira,
+    8: operations.divisao_resto
+}
 
 while programa:
-    print(menu)
-    try:
-        opcao = int(input("\n-> Opção: "))
-    except ValueError:
-        print("Digite um número válido.")
-        continue
-    
+    print(MENU)
+    opcao = inputs.ler_inteiro("-> Opção: ")
+
     if opcao == 0:
         print("\nAté a próxima! :)")
         programa = False
@@ -34,48 +43,49 @@ while programa:
     elif opcao == 1:
         print("\n=== OPERAÇÃO: ADIÇÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.soma(num1,num2)
+        resultado = operacoes[1](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 2:
         print("\n=== OPERAÇÃO: SUBTRAÇÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.subtracao(num1,num2)
+        resultado = operacoes[2](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 3:
         print("\n=== OPERAÇÃO: MULTIPLICAÇÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.multiplicacao(num1,num2)
+        resultado = operacoes[3](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 4:
         print("\n=== OPERAÇÃO: DIVISÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.divisao(num1,num2)
+        resultado = operacoes[4](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 5:
         print("\n=== OPERAÇÃO: POTENCIAÇÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.potenciacao(num1,num2)
+        resultado = operacoes[5](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 6:
         print("\n=== OPERAÇÃO: RADICIAÇÃO ===")
-        num1 = input.ler1()
-
-        resultado = operations.radiciacao(num1)
+        num1 = inputs.ler1()
+        resultado = operacoes[6](num1)
         print("Resultado: ", round(resultado, 2))
 
     elif opcao == 7:
         print("\n=== OPERAÇÃO: DIVISÃO INTEIRA ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.divisao_inteira(num1,num2)
+        resultado = operacoes[7](num1,num2)
         print("Resultado: ", resultado)
 
     elif opcao == 8:
         print("\n=== OPERAÇÃO: RESTO DA DIVISÃO ===")
         num1, num2 = inputs.ler2()
-        resultado = operations.divisao_resto(num1,num2)
+        resultado = operacoes[8](num1,num2)
         print("Resultado: ", resultado)
+    else:
+        print("Opção inválida.")
--- inputs.py
+++ inputs.py
@@ -6,3 +6,10 @@ def ler2():
     num1 = int(input("* Informe o primeiro número: "))
     num2 = int(input("* Informe o segundo número: "))
     return num1, num2
+
+def ler_inteiro(mensagem):
+    while True:
+        try:
+            return int(input(mensagem))
+        except ValueError:
+            print("Digite um número inteiro válido.")
